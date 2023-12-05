73cca87aa69d627a57e2f6f0388a77e899dcbbb6
Alan <eundong@telepix.net>
2023-12-05 23:45:02 +0900

    [UPDATE] Change Folder structure & main Code update(use cam)

--- main.py
+++ main.py
@@ -3,21 +3,37 @@ from ultralytics import YOLO
 import cv2
 
 
-model = YOLO("face-detection/yolov8n-face.pt")
+model = YOLO("model/face-detection/yolov8n-face.pt")
 
 img_path = "images/people.jpg"
 
-results = model(img_path)
+cam = cv2.VideoCapture(1)
 
-boxes = results[0].boxes
+while cam.isOpened():
+    succes, frame = cam.read()
 
-img = cv2.imread(img_path)
+    if succes:
+        results = model(frame)
 
-for box in boxes:
-    top_left_x = int(box.xyxy.tolist()[0][0])
-    top_left_y = int(box.xyxy.tolist()[0][1])
-    bottom_right_x = int(box.xyxy.tolist()[0][2])
-    bottom_right_y = int(box.xyxy.tolist()[0][3])
+        annotated_frame = results[0].plot()
+        boxes = results[0].boxes
 
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 255, 0), 2)
-    cv2.imwrite("detection.jpg", img)
+        for box in boxes:
+            top_left_x = int(box.xyxy.tolist()[0][0])
+            top_left_y = int(box.xyxy.tolist()[0][1])
+            bottom_right_x = int(box.xyxy.tolist()[0][2])
+            bottom_right_y = int(box.xyxy.tolist()[0][3])
+
+            cv2.rectangle(frame, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 255, 0), 2)
+            cv2.imshow("detection", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    else:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
+
